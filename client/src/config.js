e9fc86f9054dc900728a65fd78e46f0268aafb4b
Maciej Dzik <dzikdrums@gmail.com>
2020-02-28 14:42:33 +0100

    h

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,4 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8000/api';
+export const API_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://mernappdzik.herokuapp.com/api'
+    : 'http://localhost:8000/api';
