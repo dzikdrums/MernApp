b5875bcc5f2606822a4a8920c14c669be0f0afb9
Maciej Dzik <dzikdrums@gmail.com>
2020-02-29 11:51:44 +0100

    h

--- client/src/theme/GlobalStyle.js
+++ client/src/theme/GlobalStyle.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+  /* @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap'); */
 
   *, *::before, *::after {
     box-sizing: border-box;
